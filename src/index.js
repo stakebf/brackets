ba898d37e8c0668b9b3886c7653082fe32403f00
Volkau Aliaksey <stakebf@gmail.com>
2020-10-25 22:23:08 +0300

    feat: add solution fot task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let tempArr = [];
+
+    str.split('').map((el, indx) => {
+      for (let i = 0; i < bracketsConfig.length; i++) {
+        for (let j = 0; j < bracketsConfig[i].length; j++) {
+          if ((el == bracketsConfig[i][1]) && (tempArr[tempArr.length - 1] == bracketsConfig[i][0])) {
+              tempArr.pop();
+              break;
+          }
+          if (el == bracketsConfig[i][0]) {
+            tempArr.push(bracketsConfig[i][0]);
+            break;
+          }
+          if (tempArr.length == 0 && el == bracketsConfig[i][1]) {
+            tempArr.push(false);
+            break;
+          }
+        }
+      }
+    });
+  
+    return tempArr.length == 0 ? true : false;
 }
